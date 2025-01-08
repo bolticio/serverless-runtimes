6a069f089a949a88e147952da5fe954efa4f2d3d
anujsahu-fynd <anujsahu@gofynd.com>
2025-01-08 12:23:03 +0530

    Update handler_transformation.js

--- nodejs/handler_transformation.js
+++ nodejs/handler_transformation.js
@@ -1,27 +1,6 @@
 // Define the transformation functions
 
 // Source: Github
-function onTrack(obj) {
-    console.log("Handling track event.");
-    throw new EventNotSupportedException('anuj');
-}
-
-function onPage(obj) {
-    console.log("Handling page event.");
-    throw new InvalidEventPayloadException('sahuuu');
-}
-
-function onIdentify(obj) {
-    console.log("Handling identify event.");
-}
-function onScreen(obj) {
-    console.log("Handling screen event.");
-}
-
-function onGroup(obj) {
-    console.log("Handling group event.");
-}
-
-function onAlias(obj) {
-    console.log("Handling alias event.");
+function modifyObj(obj) {
+    
 }
